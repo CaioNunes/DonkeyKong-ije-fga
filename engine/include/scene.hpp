a23fb8386e7625a01f9d0697304658de81cb3f34
CaioNunes <caiofelipe147@gmail.com>
2017-06-05 20:09:59 -0300

    Finalize layers system

--- engine/include/scene.hpp
+++ engine/include/scene.hpp
@@ -6,6 +6,7 @@
 #include <list>
 #include "sdl2core.hpp"
 #include "gameobject.hpp"
+#include <vector>
 
 namespace engine{
 
@@ -27,13 +28,13 @@ namespace engine{
             virtual ~Scene(){}
 
             //Add and check if it already exists. If success, return true
-           virtual bool add_game_object(GameObject &obj);
+           virtual bool add_game_object(GameObject *obj);
 
             //Search for a specific GameObject by ID
-            GameObject &get_game_object(const std::string &id);
+            // GameObject &get_game_object(const std::string &id);
 
             //Erase GameObject, find it by ID and remove.
-           virtual bool remove_game_object(const std::string &id);
+        //    virtual bool remove_game_object(const std::string &id);
 
             //Initialization of scene
             virtual bool init();
@@ -44,18 +45,18 @@ namespace engine{
             //Check on map and initialize each game object. Return true if success
             virtual bool draw();
 
-           
+
             std::list<GameObject*> *get_collide_objects();
             void clear_collide_objects();
    //gamelogic
             virtual void game_logic();
-
+            void print_scene_objects();
             inline std::string name() const { return scene_name; }
 
             void update();
         protected:
             std::string scene_name;
-            std::unordered_map <std::string, GameObject *> scene_objects;
+            std::vector <GameObject *> scene_objects;
             std::list <GameObject *> collide_objects;
 
             State scene_state;
--- src/main.cpp
+++ src/main.cpp
@@ -18,48 +18,48 @@ int main(int, char **){
     std::pair<int, int> window_size(800, 600);
     Game::instance.set_properties("Donkey Kong Country",window_size);
 
-    //================================================= MENU =======================================
-    MenuScene menu("Menu");
-    Game::instance.add_scene(menu);
-
-    GameObject background_menu("background_menu");
-    ImageComponent background_menu_image(background_menu,"imageBackground_menu", "assets/sprites/menu.png");
-
-    //Objeto atrelado, ID para audio, caminho, is_music e play_on_start(default é true)
-    AudioComponent menu_music(background_menu, "menu_music", "assets/music/dk_menu.mp3", true);
-
-    background_menu.add_component(menu_music);
-    background_menu.add_component(background_menu_image);
-
-    menu.add_game_object(background_menu);
+    // //================================================= MENU =======================================
+    // MenuScene menu("Menu");
+    // Game::instance.add_scene(menu);
+    //
+    // GameObject background_menu("background_menu");
+    // ImageComponent background_menu_image(background_menu,"imageBackground_menu", "assets/sprites/menu.png",1);
+    //
+    // //Objeto atrelado, ID para audio, caminho, is_music e play_on_start(default é true)
+    // AudioComponent menu_music(background_menu, "menu_music", "assets/music/dk_menu.mp3", true);
+    //
+    // background_menu.add_component(menu_music);
+    // background_menu.add_component(background_menu_image);
+    //
+    // menu.add_game_object(background_menu);
 
     //================================================= STAGE =======================================
     StageScene stage("WaterStage");
     Game::instance.add_scene(stage);
 
-    GameObject music_background("music_background");
+    GameObject music_background("music_background", 4);
 
-    GameObject background_stage("background_stage");
-    ImageComponent background_stage_image(background_stage,"imageBackground_stage", "assets/sprites/background_stage.png");
+    GameObject background_stage("background_stage", 1);
+    ImageComponent background_stage_image(background_stage,"imageBackground_stage", "assets/sprites/background_stage.png", 3);
 
     AudioComponent stage_music(music_background, "stage_music", "assets/music/dk_stage.mp3", true);
 
     music_background.add_component(stage_music);
     background_stage.add_component(background_stage_image);
 
-    GameObject maps("maps");
-    ImageComponent maps_stage(maps, "maps_stage", "assets/sprites/maps_resized.png");
+    GameObject maps("maps", 2);
+    ImageComponent maps_stage(maps, "maps_stage", "assets/sprites/maps_resized.png",2);
 
     maps.main_positionY = -2220;
 
     maps.add_component(maps_stage);
 
-    GameObject donkey_player("donkey_player");
+    GameObject donkey_player("donkey_player", 3);
     AnimationControllerComponent donkeyCtrl(donkey_player, "animationControllerDonkey");
 
     PlayerController player_controller(donkey_player, "donkey_player");
 
-    Animation donkey_swim(donkey_player, "donkey_swim", "assets/sprites/donkey_resized.png", 1790/15 , 90, 15);
+    Animation donkey_swim(donkey_player, "donkey_swim", "assets/sprites/donkey_resized.png", 1, 1790/15 , 90, 15);
     donkey_swim.setDelay(100);
 
     donkeyCtrl.add_animation("donkey_swim", donkey_swim);
@@ -70,11 +70,12 @@ int main(int, char **){
 
     donkey_player.main_positionY = 305;
 
-    stage.add_game_object(music_background);
-    stage.add_game_object(maps);
-    stage.add_game_object(donkey_player);
-    stage.add_game_object(background_stage);
+    stage.add_game_object(&music_background);
+    stage.add_game_object(&maps);
+    stage.add_game_object(&donkey_player);
+    stage.add_game_object(&background_stage);
 
+    stage.print_scene_objects();
     Game::instance.run();
 
     return 0;
